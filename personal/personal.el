40ecc0bd066659b67c8b6a009203546e43e7c2e2
yousack <love.the.quiz.yousack@gmail.com>
2015-05-27 14:54:39 +0900

    いろんなモードを追加した

--- personal/personal.el
+++ personal/personal.el
@@ -5,11 +5,11 @@
 ;;; Code:
 
 (server-start)
-(mac-translate-from-yen-to-backslash)
+;; (mac-translate-from-yen-to-backslash)
 (global-linum-mode)
 (electric-indent-mode -1)
 (disable-theme 'zenburn)
-(load-theme 'material)
+(load-theme 'flatui)
 (setq default-input-method "japanese-mozc")
 (defvar mozc-helper-program-name "/usr/local/bin/mozc_emacs_helper")
 (require 'mozc-popup)
@@ -20,26 +20,63 @@
       (set-scroll-bar-mode nil)
       (setq inhibit-startup-screen t)
       (set-face-attribute 'default nil
-                          :family "Inconsolata"
+                          :family "Myrica M"
                           :height 180)
-      (set-fontset-font
-       nil 'japanese-jisx0208
-       (font-spec :family "Hiragino Kaku Gothic ProN"))
-      (setq face-font-rescale-alist
-            '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)))
       (set-frame-parameter nil 'alpha 90)
 ))
+
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; visual regexp
+(require 'visual-regexp-steroids)
+(define-key global-map (kbd "M-%") 'vr/replace)
+
+;; smart-compile
+(require 'smart-compile)
+(bind-key "C-c c" 'smart-compile)
+(bind-key "C-c C-c" "C-c c C-m")
+
+;; company
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; Ruby
 (add-hook 'ruby-mode-hook 'robe-mode)
 (push 'company-robe company-backends)
 
+;; Python
+(pyenv-mode)
+
+;; C++
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; C#
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+(setq omnisharp-server-executable-path "/Users/yousack/src/OmniSharpServer/OmniSharp/bin/Debug/Omnisharp.exe")
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-omnisharp))
+
+;; Org
 (require 'org)
+(setq org-src-fontify-natively t)
 (require 'ox-latex)
+(require 'ox-bibtex)
 (setq org-latex-packages-alist
       '(
         ("" "bm")
-        ("" "authblk")
         ("" "url")
         ("yu-osx" "luatexja-preset")
         ))
